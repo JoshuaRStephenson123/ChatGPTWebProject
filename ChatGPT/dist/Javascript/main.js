3f677f0c14e0d8f097fb7c923ce481fe016667e4
Joshua Stephenson <117106576+JoshuaRStephenson123@users.noreply.github.com>
2023-06-20 20:01:23 +0100

    Add files via upload

---
+++ ChatGPT/dist/Javascript/main.js
@@ -0,0 +1,15 @@
+const loading = document.querySelector(".loading");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+function init() {
+
+    const characters = document.querySelectorAll('.character');
+
+    console.log(characters);
+    
+
+}
+
+alert(init)
